4aa6d567d9bfead4ca3030f1a9dcb727c016c988
fy88fy <30521047+fy88fy@users.noreply.github.com>
2018-02-21 21:29:07 +0800

    Add files via upload

---
+++ 3.quit代码.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @time   :2018/1/6 16:55
+# @Author :FengXiaoqing
+# @file   :test.py
+#!/usr/bin/python
+
+
+
+#
+# l1 = ['a','b','c','d']
+# l2 = [1,2,3,4]
+# a = zip(l1,l2)
+# print(a)
+
